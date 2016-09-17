66ee7980813050f243318e9721fb0b2e4f332a39
joelfi <mbp.local>
2016-09-17 18:52:45 +0300

    🔨 Locate user on page load, add marker animation, change max zoom

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -30,6 +30,8 @@ $(function() {
         map: null,
         settings: {},
         clickedMarker: null,
+        scanCircle: null,
+        working: false,
         hiddenPokemon: [],
         pokemons: $.parseJSON("{\"1\":\"Bulbasaur\",\"2\":\"Ivysaur\",\"3\":\"Venusaur\",\"4\":\"Charmander\",\"5\":\"Charmeleon\",\"6\":\"Charizard\",\"7\":\"Squirtle\",\"8\":\"Wartortle\",\"9\":\"Blastoise\",\"10\":\"Caterpie\",\"11\":\"Metapod\",\"12\":\"Butterfree\",\"13\":\"Weedle\",\"14\":\"Kakuna\",\"15\":\"Beedrill\",\"16\":\"Pidgey\",\"17\":\"Pidgeotto\",\"18\":\"Pidgeot\",\"19\":\"Rattata\",\"20\":\"Raticate\",\"21\":\"Spearow\",\"22\":\"Fearow\",\"23\":\"Ekans\",\"24\":\"Arbok\",\"25\":\"Pikachu\",\"26\":\"Raichu\",\"27\":\"Sandshrew\",\"28\":\"Sandslash\",\"29\":\"Nidoran F\",\"30\":\"Nidorina\",\"31\":\"Nidoqueen\",\"32\":\"Nidoran M\",\"33\":\"Nidorino\",\"34\":\"Nidoking\",\"35\":\"Clefairy\",\"36\":\"Clefable\",\"37\":\"Vulpix\",\"38\":\"Ninetales\",\"39\":\"Jigglypuff\",\"40\":\"Wigglytuff\",\"41\":\"Zubat\",\"42\":\"Golbat\",\"43\":\"Oddish\",\"44\":\"Gloom\",\"45\":\"Vileplume\",\"46\":\"Paras\",\"47\":\"Parasect\",\"48\":\"Venonat\",\"49\":\"Venomoth\",\"50\":\"Diglett\",\"51\":\"Dugtrio\",\"52\":\"Meowth\",\"53\":\"Persian\",\"54\":\"Psyduck\",\"55\":\"Golduck\",\"56\":\"Mankey\",\"57\":\"Primeape\",\"58\":\"Growlithe\",\"59\":\"Arcanine\",\"60\":\"Poliwag\",\"61\":\"Poliwhirl\",\"62\":\"Poliwrath\",\"63\":\"Abra\",\"64\":\"Kadabra\",\"65\":\"Alakazam\",\"66\":\"Machop\",\"67\":\"Machoke\",\"68\":\"Machamp\",\"69\":\"Bellsprout\",\"70\":\"Weepinbell\",\"71\":\"Victreebel\",\"72\":\"Tentacool\",\"73\":\"Tentacruel\",\"74\":\"Geodude\",\"75\":\"Graveler\",\"76\":\"Golem\",\"77\":\"Ponyta\",\"78\":\"Rapidash\",\"79\":\"Slowpoke\",\"80\":\"Slowbro\",\"81\":\"Magnemite\",\"82\":\"Magneton\",\"83\":\"Farfetch'd\",\"84\":\"Doduo\",\"85\":\"Dodrio\",\"86\":\"Seel\",\"87\":\"Dewgong\",\"88\":\"Grimer\",\"89\":\"Muk\",\"90\":\"Shellder\",\"91\":\"Cloyster\",\"92\":\"Gastly\",\"93\":\"Haunter\",\"94\":\"Gengar\",\"95\":\"Onix\",\"96\":\"Drowzee\",\"97\":\"Hypno\",\"98\":\"Krabby\",\"99\":\"Kingler\",\"100\":\"Voltorb\",\"101\":\"Electrode\",\"102\":\"Exeggcute\",\"103\":\"Exeggutor\",\"104\":\"Cubone\",\"105\":\"Marowak\",\"106\":\"Hitmonlee\",\"107\":\"Hitmonchan\",\"108\":\"Lickitung\",\"109\":\"Koffing\",\"110\":\"Weezing\",\"111\":\"Rhyhorn\",\"112\":\"Rhydon\",\"113\":\"Chansey\",\"114\":\"Tangela\",\"115\":\"Kangaskhan\",\"116\":\"Horsea\",\"117\":\"Seadra\",\"118\":\"Goldeen\",\"119\":\"Seaking\",\"120\":\"Staryu\",\"121\":\"Starmie\",\"122\":\"Mr. Mime\",\"123\":\"Scyther\",\"124\":\"Jynx\",\"125\":\"Electabuzz\",\"126\":\"Magmar\",\"127\":\"Pinsir\",\"128\":\"Tauros\",\"129\":\"Magikarp\",\"130\":\"Gyarados\",\"131\":\"Lapras\",\"132\":\"Ditto\",\"133\":\"Eevee\",\"134\":\"Vaporeon\",\"135\":\"Jolteon\",\"136\":\"Flareon\",\"137\":\"Porygon\",\"138\":\"Omanyte\",\"139\":\"Omastar\",\"140\":\"Kabuto\",\"141\":\"Kabutops\",\"142\":\"Aerodactyl\",\"143\":\"Snorlax\",\"144\":\"Articuno\",\"145\":\"Zapdos\",\"146\":\"Moltres\",\"147\":\"Dratini\",\"148\":\"Dragonair\",\"149\":\"Dragonite\",\"150\":\"Mewtwo\",\"151\":\"Mew\"}"),
 
@@ -80,22 +82,26 @@ $(function() {
             });
         },
 
+        locateUser: function() {
+            navigator.geolocation.getCurrentPosition((location) => {
+                this.map.setView(new L.LatLng(location.coords.latitude, location.coords.longitude), 15);
+
+                L.marker([location.coords.latitude, location.coords.longitude], {
+                    icon: L.icon.glyph({
+                        prefix: 'mdi',
+                        glyph: 'account-location',
+                        background: false,
+                        glyphSize: '15pt'
+                    })
+                }).addTo(this.map);
+            });
+        },
+
         initListeners: function() {
             $("#settings-trigger").leanModal();
 
             $("#locate-trigger").on('click', () => {
-                navigator.geolocation.getCurrentPosition((location) => {
-            		this.map.setView(new L.LatLng(location.coords.latitude, location.coords.longitude), 15);
-
-                    L.marker([location.coords.latitude, location.coords.longitude], {
-                        icon: L.icon.glyph({
-                            prefix: 'mdi',
-                            glyph: 'account-location',
-                            background: false,
-                            glyphSize: '15pt'
-                        })
-                    }).addTo(this.map);
-            	});
+                this.locateUser();
             });
 
             $("#save-settings").on('click', () => {
@@ -119,14 +125,24 @@ $(function() {
             });
 
             this.map.on('click', (e) => {
-                if(this.clickedMarker != null) this.map.removeLayer(this.clickedMarker);
-                this.clickedMarker = L.marker(e.latlng, {
-                    icon: L.icon.glyph({
-                        prefix: 'mdi',
-                        glyph: 'map-marker',
-                        background: false,
-                        glyphSize: '15pt'
-                    })
+                if(!this.working) {
+                    if(this.clickedMarker != null) this.map.removeLayer(this.clickedMarker);
+                    this.clickedMarker = L.marker(e.latlng, {
+                        icon: L.icon.glyph({
+                            prefix: 'mdi',
+                            glyph: 'map-marker',
+                            background: false,
+                            glyphSize: '15pt'
+                        })
+                    }).addTo(this.map);
+                }
+
+                if(this.scanCircle != null) this.map.removeLayer(this.scanCircle);
+                this.scanCircle = L.circle(e.latlng, 70, {
+                    color: 'red',
+                    fillColor: '#f03',
+                    fillOpacity: 0.5,
+                    className: 'scanCircle'
                 }).addTo(this.map);
             });
 
@@ -151,9 +167,11 @@ $(function() {
             this.map.setView(new L.LatLng(34.0095897345215,-118.49791288375856),16);
             this.map.addLayer(new L.TileLayer(osm.url, {
                 minZoom: 2,
-                maxZoom: 17,
+                maxZoom: 20,
                 attribution: osm.attribution
             }));
+
+            this.locateUser();
         },
 
         initData: function(callback) {
