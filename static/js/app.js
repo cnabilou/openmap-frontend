7e141f38048621bcfccb44b54657c99b872b3b9b
Joel J <joelj@MacBook-Pro.local>
2016-09-19 11:30:58 +0300

    💄 Change attribution and it's position and make Pokémon icons bigger

--- static/js/app.js
+++ static/js/app.js
@@ -215,11 +215,17 @@ $(function() {
 
         initMap: function() {
             this.map = new L.Map('map', {
-                zoomControl: false
+                zoomControl: false,
+                attributionControl: false
             });
 
             // start in Santa Monica
             this.map.setView(new L.LatLng(34.0095897345215,-118.49791288375856),16);
+
+            L.control.attribution({
+                position: 'topright',
+                prefix: 'Leaflet | Map tiles hosted by <a href="https://fastpokemap.se>FPM</a> | © OpenStreetMap contributors'
+            }).addTo(this.map);
 
             var gl = L.mapboxGL({
                 accessToken: "opm",
