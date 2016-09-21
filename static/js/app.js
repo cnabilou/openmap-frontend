685385f22093256f391ee7adf806ebd51be60200
Joel J <joelj@MacBook-Pro.local>
2016-09-21 20:06:41 +0300

    🔧 Fixes

--- static/js/app.js
+++ static/js/app.js
@@ -1,32 +1,6 @@
 (function(a){(jQuery.browser=jQuery.browser||{}).mobile=/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))})(navigator.userAgent||navigator.vendor||window.opera);
 
 $(function() {
-    function prettyTime(seconds) {
-        var minutes = Math.floor(seconds / 60);
-        var remainingSeconds = seconds - minutes * 60;
-
-        return ("0" + minutes).slice(-2) + ":" + ("0" + remainingSeconds).slice(-2);
-    }
-
-    /* custom leaflet plugins */
-    L.PokemonIcon = L.Icon.extend({
-    	options: {},
-
-    	initialize: function (options) {
-    		L.Util.setOptions(this, options);
-    	},
-
-    	createIcon: function () {
-    		var div = document.createElement('div');
-    		div.innerHTML = this.options.html;
-    		return div;
-    	},
-
-    	createShadow: function () {
-    		return null;
-    	}
-    });
-
     /* app */
     var app = {
         map: null,
@@ -52,6 +26,13 @@ $(function() {
             timeout: 33 // seconds
         },
         pokemons: {},
+
+        prettyTime: function(seconds) {
+            var minutes = Math.floor(seconds / 60);
+            var remainingSeconds = seconds - minutes * 60;
+
+            return ("0" + minutes).slice(-2) + ":" + ("0" + remainingSeconds).slice(-2);
+        },
 
         togglePokemonDiv: function(div, toggleOverride) {
             if(typeof toggleOverride == 'undefined') {
@@ -90,6 +71,8 @@ $(function() {
                 self.locateUser();
 
                 self.initData(function() {
+                    console.log(self.pokemons);
+
                     $.each(self.pokemons, function(id, name) {
                         $(".pokemon-list").append("<div class=\"pokemon-list-pokemon\" data-pokemon-id=\"" + id + "\" data-selected=\"true\"><div class=\"pokemon-list-pokemon-image pi pi-" + id + "\"></div><div class=\"pokemon-list-pokemon-name\">" + name + "</div></div>");
                     });
@@ -268,7 +251,7 @@ $(function() {
 
                         $(this).attr("data-expired", true);
                     } else {
-                        $(this).text(prettyTime(timeRemaining));
+                        $(this).text(self.prettyTime(timeRemaining));
                     }
                 });
             }, 1000);
@@ -413,31 +396,9 @@ $(function() {
                 $.each(data.MapObjects, function(k, mapObject) {
                     if(mapObject.Type == 1) {
                         if(self.objectUUIDs.pokemons.indexOf(mapObject.Id) == -1) {
-                            /*var marker = L.marker([mapObject.Lat, mapObject.Lng], {
-                                icon: new L.PokemonIcon({
-                                    html: '<div class="map-pokemon" data-pokemonid="' + mapObject.PokemonId + '">' +
-                                            '<div class="pi pi-' + mapObject.PokemonId + ' pi-small" style="position: absolute; margin-top: -22px; margin-left: -22px"></div>' +
-                                            '<div class="map-pokemon timer" data-expired="false" data-expiry="' + mapObject.Expiry + '">' +
-                                                prettyTime(mapObject.Expiry - Math.round(new Date() / 1000)) +
-                                            '</div>' +
-                                          '</div>'
-                                })
-                            }).addTo(self.map);
-
-                            marker.addEventListener("mouseover", function() {
-                                var markers = document.getElementsByClassName("leaflet-marker-pane")[0].childNodes;
-                                self.map.eachLayer(function(layer) {
-                                    if(typeof layer._icon !== 'undefined')
-                                        if(typeof layer._icon.className !== 'undefined')
-                                            if(layer._icon.className.indexOf('leaflet-marker-icon'))
-                                                layer.setZIndexOffset(1);
-                                });
-                                this.setZIndexOffset(9999);
-                            });*/
-
                             var marker = document.createElement('div');
                             marker.className = 'map-pokemon';
-                            marker.innerHTML = '<div class="pi pi-small pi-' + mapObject.PokemonId + '"></div><div class="map-pokemon-timer" data-expired="false" data-expiry="' + mapObject.Expiry + '">' + prettyTime(mapObject.Expiry - Math.round(new Date() / 1000)) + '</div>';
+                            marker.innerHTML = '<div class="pi pi-small pi-' + mapObject.PokemonId + '"></div><div class="map-pokemon-timer" data-expired="false" data-expiry="' + mapObject.Expiry + '">' + self.prettyTime(mapObject.Expiry - Math.round(new Date() / 1000)) + '</div>';
 
                             new mapboxgl.Marker(marker)
                                 .setLngLat([+mapObject.Lng, +mapObject.Lat])
